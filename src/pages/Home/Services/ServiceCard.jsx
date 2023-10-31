dc27c76daee569fcb8cf2da8d8c5b4f92443df50
Shuvo <shuvoorba28@gmail.com>
2023-10-31 02:28:11 -0700

    services section added

---
+++ src/pages/Home/Services/ServiceCard.jsx
@@ -0,0 +1,27 @@
+import { BsArrowRight } from 'react-icons/bs';
+
+const ServiceCard = ({service}) => {
+
+    const {title, img, price} = service || {}; 
+
+  return (
+    <div className="card w-96 bg-base-100 shadow-xl">
+      <figure className="px-10 pt-10">
+        <img
+          src={img}
+          alt="Shoes"
+          className="rounded-xl"
+        />
+      </figure>
+      <div className="card-body">
+        <h2 className="card-title">{title}</h2> 
+        <div className="card-actions">
+          <p className="font-semibold text-yellow-600">Price: $ {price}</p>
+          <BsArrowRight  className="font-bold text-yellow-600" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ServiceCard;
--- src/pages/Home/Services/Services.jsx
+++ src/pages/Home/Services/Services.jsx
@@ -1,16 +1,33 @@
-
+import { useEffect, useState } from "react";
+import ServiceCard from "./ServiceCard";
 
 const Services = () => {
-    
-    return (
-        <div className="mt-4">
-            <div className="text-center">
-                <h3 className="text-2xl font-bold text-[#FF3811]">Service</h3>
-                <h2 className="text-5xl">Our service area</h2>
-                <p>the majority have suffered alteration in some form, by injected humour, or <br /> randomised  words which don't look even slightly believable. </p>
-            </div>
-        </div>
-    );
+  const [services, setServices] = useState([]);
+
+  useEffect(() => {
+    fetch("services.json")
+      .then((res) => res.json())
+      .then((data) => setServices(data));
+  }, []);
+
+  return (
+    <div className="mt-4">
+      <div className="text-center">
+        <h3 className="text-2xl font-bold text-[#FF3811]">Service</h3>
+        <h2 className="text-5xl">Our service area</h2>
+        <p>
+          the majority have suffered alteration in some form, by injected
+          humour, or <br /> randomised words which don't look even slightly
+          believable.{" "}
+        </p>
+      </div>
+      <div className="grid grid-cols-1 mb-10 mg:grid-cols-2 lg:grid-cols-3 gap-3">
+        {services.map((service) => (
+          <ServiceCard key={service._id} service={service}></ServiceCard>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Services;

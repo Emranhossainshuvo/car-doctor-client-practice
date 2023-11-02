86ded9fe2ea4b44ad27995484a4224e9b28fdaef
Shuvo <shuvoorba28@gmail.com>
2023-11-02 14:23:01 -0700

    token removed from client cookie after logout

--- src/Providers/AuthProvider.jsx
+++ src/Providers/AuthProvider.jsx
@@ -1,58 +1,81 @@
 import { createContext, useEffect, useState } from "react";
-import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut } from "firebase/auth";
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signOut,
+} from "firebase/auth";
 import app from "../firebase/firebase.config";
 import Swal from "sweetalert2";
+import axios from "axios";
 
+export const AuthContext = createContext();
+const auth = getAuth(app);
 
-export const AuthContext = createContext(); 
-const auth = getAuth(app)
+const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
 
-const AuthProvider = ({children}) => {
-    const [user, setUser] = useState(null); 
-    const [loading, setLoading] = useState(true); 
+  const createUser = (email, password) => {
+    setLoading(true);
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
 
-    const createUser = (email, password) => {
-        setLoading(true); 
-        return createUserWithEmailAndPassword(auth, email, password)
-    }
+  const logOut = () => {
+    setLoading(true);
+    return signOut(auth);
+  };
 
-    const logOut = () => {
-        setLoading(true)
-        return signOut(auth); 
-    }
+  const signIn = (email, password) => {
+    setLoading(true);
+    return signInWithEmailAndPassword(auth, email, password);
+  };
 
-    const signIn = (email, password) => {
-        setLoading(true)
-        return signInWithEmailAndPassword(auth, email, password)
-    }
+  useEffect(() => {
+    const unSubscribe = onAuthStateChanged(auth, (currentUser) => {
+        const userEmail = currentUser?.email || user?.email;
+        const loggedUser = { email: userEmail };
+        setUser(currentUser);
+      console.log("current user", currentUser);
+      // if(!currentUser){
+      //     Swal.fire('Logged out!')
+      // }
+      setLoading(false);
+      if (currentUser) {
+        axios
+          .post("http://localhost:5000/jwt", loggedUser, {
+            withCredentials: true,
+          })
+          .then((res) => {
+            console.log("token response", res.data);
+          });
+      } else {
+        axios
+          .post("http://localhost:5000/logout", loggedUser, {
+            withCredentials: true,
+          })
+          .then((res) => {
+            console.log(res.data);
+          });
+      }
+    });
+    return () => {
+      return unSubscribe();
+    };
+  }, []);
 
-    useEffect( () => {
-        const unSubscribe = onAuthStateChanged(auth, currentUser => {
-            setUser(currentUser)
-            // if(!currentUser){
-            //     Swal.fire('Logged out!')
-            // }
-            setLoading(false)
-        }); 
-        return () => {
-            return unSubscribe(); 
-        }
+  const authData = {
+    user,
+    logOut,
+    loading,
+    createUser,
+    signIn,
+  };
 
-    }, [])
-
-    const authData = {
-        user, 
-        logOut,
-        loading, 
-        createUser, 
-        signIn
-    }
-
-    return (
-        <AuthContext.Provider value={authData}>
-            {children}
-        </AuthContext.Provider>
-    );
+  return (
+    <AuthContext.Provider value={authData}>{children}</AuthContext.Provider>
+  );
 };
 
 export default AuthProvider;
